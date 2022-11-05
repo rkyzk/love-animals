928894306bb13a94d42d548f3cf226deb2188edc
rkyzk <reikoyazaki@yahoo.com>
2022-11-05 01:54:51 +0000

    list core functions

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+  assignImgToCards();
+});
+
+/**
+ * assign images to the cards
+ */
+ function assignImgToCards() {
+
+  }
+
+/**
+ * flip cards on click  
+ * */  
+function flipCard() {
+
+}
+
+/**
+ * check if two flipped cards are the same.
+ */
+function check() {
+
+}
+
