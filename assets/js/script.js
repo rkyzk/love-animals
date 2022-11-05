e80414b2e09d9e9d31a3a1f73279f3e40d058123
rkyzk <reikoyazaki@yahoo.com>
2022-11-05 02:00:54 +0000

    add functions to assign images to the cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,22 @@ document.addEventListener("DOMContentLoaded", function() {
 /**
  * assign images to the cards
  */
- function assignImgToCards() {
-
+function assignImgToCards() {
+  let images = ["elephant.png", "flamingo.png", "giraffe.png", "lion.png", "savanna-tree.jpg", "zebra.png",
+    "elephant.png", "flamingo.png", "giraffe.png", "lion.png", "savanna-tree.jpg", "zebra.png"];
+  shuffle(images);
+  for (i = 0; i < images.length; i++) {
+    cardImgs.push(images[i]);
   }
+  return cardImgs;
+}
+  
+function shuffle(images) {
+  for (let i = 0; i < images.length; i++) {
+    let j = Math.floor(Math.random() * images.length);
+    [images[i], images[j]] = [images[j], images[i]];
+  }
+}
 
 /**
  * flip cards on click  
