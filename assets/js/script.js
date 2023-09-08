60cf5223b0dbdd3bf523a3b98541a495528ef992
rkyzk <reikoyazaki@yahoo.com>
2023-09-08 06:57:41 +0000

    change the instruction back to English

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,14 +16,14 @@ let timer;
 /** The index of the star images */
 let k = 0;
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Assign an animal image to each card
   cardImgs = assignImgToCards();
   // Change the color of the card at mouseover/out
-  let cards = document.getElementsByClassName("cards");   
+  let cards = document.getElementsByClassName("cards");
   for (let card of cards) {
     card.addEventListener("mouseover", changeColor);
-    card.addEventListener("mouseout", changeColorBack); 
+    card.addEventListener("mouseout", changeColorBack);
   }
   addFlipCardsEventListener();
 });
@@ -33,18 +33,20 @@ document.addEventListener("DOMContentLoaded", function() {
  * @return Array of animal images in a randomized order
  */
 function assignImgToCards() {
-  let images = ["elephant.png", "flamingo.png", "giraffe.png", "lion.png", 
-    "savanna-tree.jpg", "zebra.png", "elephant.png", "flamingo.png", 
-    "giraffe.png", "lion.png", "savanna-tree.jpg", "zebra.png"];
+  let images = ["elephant.png", "flamingo.png", "giraffe.png", "lion.png",
+    "savanna-tree.jpg", "zebra.png", "elephant.png", "flamingo.png",
+    "giraffe.png", "lion.png", "savanna-tree.jpg", "zebra.png"
+  ];
   images = shuffle(images);
   for (i = 0; i < images.length; i++) {
     cardImgs.push(images[i]);
   }
   return cardImgs;
 }
-  
+
 function shuffle(images) {
-  let currentIndex = images.length, randomIndex;
+  let currentIndex = images.length,
+    randomIndex;
 
   // While there are elements left to shuffle 
   while (currentIndex != 0) {
@@ -53,19 +55,18 @@ function shuffle(images) {
     currentIndex--;
 
     // And swap it with the current element.
-    [images[currentIndex], images[randomIndex]] = 
-      [images[randomIndex], images[currentIndex]];
+    [images[currentIndex], images[randomIndex]] = [images[randomIndex], images[currentIndex]];
   }
   return images;
 }
 
 /**
  * Flip cards on click  
- */  
+ */
 function flipCards(event) {
   /** Condition: the card shows the blue side (not the image side), 
     and a card has been clicked (not the gaps) */
-  if (event.target.getAttribute('src') === null && event.target.tagName === 'DIV') { 
+  if (event.target.getAttribute('src') === null && event.target.tagName === 'DIV') {
     // start running the timer on first click and disable the function after the first click
     if (firstClick === true) {
       firstClick = false;
@@ -75,20 +76,20 @@ function flipCards(event) {
     let num = event.target.getAttribute('id').substr(4);
     let image = document.createElement('img');
     image.src = 'assets/images/' + cardImgs[num - 1];
-    image.alt = cardImgs[num - 1].substr(0, cardImgs[num -1].lastIndexOf('.'));
+    image.alt = cardImgs[num - 1].substr(0, cardImgs[num - 1].lastIndexOf('.'));
     image.style.backgroundColor = "beige";
     image.style.width = '100%';
     image.style.height = '100%';
     event.target.appendChild(image);
     event.target.classList.add('flipped');
-  }  
+  }
 
   // remove the functions to change colors at mouseover/mouseout from the flipped card
-  event.target.removeEventListener('mouseover', changeColor); 
-  event.target.removeEventListener('mouseout', changeColorBack);  
+  event.target.removeEventListener('mouseover', changeColor);
+  event.target.removeEventListener('mouseout', changeColorBack);
 
   // disable eventListener (flipCards) from all the cards after two cards have been flipped
-  let flipped = document.getElementsByClassName('flipped');   
+  let flipped = document.getElementsByClassName('flipped');
   if (flipped.length === 2) {
     removeFlipCardsEListener();
     // check if the two cards are the same
@@ -101,19 +102,19 @@ function flipCards(event) {
  * and handle them accordingly.
  */
 function check(flipped0, flipped1) {
-  let cards = document.getElementsByClassName("cards");  
+  let cards = document.getElementsByClassName("cards");
   // if the two cards are the same, let them disappear after half a second
-  if (flipped0.firstElementChild.getAttribute('src') === 
+  if (flipped0.firstElementChild.getAttribute('src') ===
     flipped1.firstElementChild.getAttribute('src') &&
     seconds !== 0) {
-      setTimeout (function () {
-        flipped0.style.visibility = "hidden";
-        flipped1.style.visibility = "hidden";
-        getStar();
-        // if all 12 cards have disappeared, display a reward message by calling reward()
-        let count = 0;
-        for (let card of cards) {
-          if (card.style.visibility === "hidden") {
+    setTimeout(function () {
+      flipped0.style.visibility = "hidden";
+      flipped1.style.visibility = "hidden";
+      getStar();
+      // if all 12 cards have disappeared, display a reward message by calling reward()
+      let count = 0;
+      for (let card of cards) {
+        if (card.style.visibility === "hidden") {
           count++;
         }
       }
@@ -126,15 +127,15 @@ function check(flipped0, flipped1) {
     }, 500);
   } else {
     // if the two cards don't match, flip them back after 1 second.
-    setTimeout (function () {
+    setTimeout(function () {
       flipped0.removeChild(flipped0.firstElementChild);
       flipped1.removeChild(flipped1.firstElementChild);
       flipped0.style.backgroundColor = "lightsteelblue";
       flipped1.style.backgroundColor = "lightsteelblue";
       flipped0.addEventListener("mouseover", changeColor);
-      flipped0.addEventListener("mouseout", changeColorBack); 
+      flipped0.addEventListener("mouseout", changeColorBack);
       flipped1.addEventListener("mouseover", changeColor);
-      flipped1.addEventListener("mouseout", changeColorBack); 
+      flipped1.addEventListener("mouseout", changeColorBack);
       // let the flip-card function resume
       addFlipCardsEventListener();
     }, 1000);
@@ -144,14 +145,14 @@ function check(flipped0, flipped1) {
 }
 
 function addFlipCardsEventListener() {
-  let allCards = document.querySelector('#cards-wrapper'); 
+  let allCards = document.querySelector('#cards-wrapper');
   allCards.addEventListener("click", flipCards);
 }
 
 function changeColor(event) {
   event.target.style.backgroundColor = "lightslategray";
 }
-  
+
 function changeColorBack(event) {
   event.target.style.backgroundColor = "lightsteelblue";
 }
@@ -165,11 +166,11 @@ function getStar() {
   stars[k].setAttribute("src", "assets/images/yellow-star.jpg");
   k++;
 }
-  
+
 /**
  * Display a reward message when the user completes the game.
  */
-function reward() {    
+function reward() {
   clearInterval(timer);
   let countdown = document.getElementById('countdown');
   countdown.remove();
@@ -182,10 +183,10 @@ function reward() {
   let graphics = document.createElement('img');
   graphics.src = 'assets/images/savanna-forest.jpg';
   graphics.alt = 'savanna forest';
-  myNode.append(message);   
-  myNode.append(graphics);   
+  myNode.append(message);
+  myNode.append(graphics);
   displayResetButton();
-} 
+}
 
 /**
  * display a reset button for refreshing the page
@@ -214,12 +215,12 @@ function updateCountdown() {
   if (seconds === 0) {
     clearInterval(timer);
     removeFlipCardsEListener();
-    removeColorChange(); 
+    removeColorChange();
     // display a comment saying "Time's up" after 0.4 seconds
-    setTimeout(function() {    
+    setTimeout(function () {
       countdownEl.style.width = "160px";
       countdownEl.innerHTML = "Time's up!";
-      countdownEl.style.color = "red";     
+      countdownEl.style.color = "red";
       displayResetButton();
     }, 400);
   }
@@ -229,10 +230,10 @@ function updateCountdown() {
  * Disable the function to change color at mouseover/out  
  */
 function removeColorChange() {
-  let cards = document.getElementsByClassName("cards");   
+  let cards = document.getElementsByClassName("cards");
   for (let card of cards) {
     card.removeEventListener("mouseover", changeColor);
-    card.removeEventListener("mouseout", changeColorBack); 
+    card.removeEventListener("mouseout", changeColorBack);
   }
 }
 
@@ -240,7 +241,7 @@ function removeColorChange() {
  * Add flip-card function to all the cards
  */
 function addFlipCardsEventListener() {
-  let allCards = document.querySelector('#cards-wrapper'); 
+  let allCards = document.querySelector('#cards-wrapper');
   allCards.addEventListener("click", flipCards);
 }
 
@@ -248,6 +249,6 @@ function addFlipCardsEventListener() {
  * Remove flip-card function from all the cards
  */
 function removeFlipCardsEListener() {
-  let allCards = document.querySelector('#cards-wrapper'); 
+  let allCards = document.querySelector('#cards-wrapper');
   allCards.removeEventListener("click", flipCards);
 }
